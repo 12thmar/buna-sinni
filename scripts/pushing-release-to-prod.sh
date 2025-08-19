a35ef35967a8dea0ca8f5025d134e84da1dd3e3a
Seid <seid.adem@gmail.com>
2025-08-18 22:10:43 -0400

    fixing release script

--- scripts/pushing-release-to-prod.sh
+++ scripts/pushing-release-to-prod.sh
@@ -1,6 +1,6 @@
-( cd frontend && npm ci --include=dev && npm run build )
 git fetch origin
 git checkout release/2025-08-17
+( cd frontend && npm ci --include=dev && npm run build )
 git merge origin/prod      # or: git rebase origin/prod
 # resolve any conflicts
  # quick sanity build
