df00f5d2662be28704520dae65f8e3d6c4293282
Seid <seid.adem@gmail.com>
2025-08-18 22:16:04 -0400

    satisfying email

--- scripts/pushing-release-to-prod.sh
+++ scripts/pushing-release-to-prod.sh
@@ -1,3 +1,5 @@
+#git branch -D release/2025-08-18
+#git push origin --delete release/2025-08-18
 git fetch origin
 git checkout release/2025-08-17
 ( cd frontend && npm ci --include=dev && npm run build )
